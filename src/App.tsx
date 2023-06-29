18d0386f9c831029feef8fba263328f33f402cb7
Marcelo Joia <sitejoia@hotmail.com>
2023-06-29 13:54:35 -0300

    Rotas aninhadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
-    <nav
+    <>
+      <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
@@ -13,6 +14,8 @@ function App() {
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      <Outlet />
+    </>
   )
 }
 
