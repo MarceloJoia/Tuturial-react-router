a98d3a96a37b59f718075337ebf63df0c0006f83
Marcelo Joia <sitejoia@hotmail.com>
2023-06-29 18:29:08 -0300

    Lendo Parâmetros de URL

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Expenses from './routes/Expenses/index.tsx';
 import Invoices from './routes/Invoices/index.tsx';
 import NotFound from './routes/NotFound/index.tsx';
+import Invoice from './routes/Invoices/Invoice/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
 
-        {/* //=> Sub conteúdo de menu NotFound 404 */}
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />{/* //=> Sub conteúdo de menu NotFound 404 */}
 
       </Route>
     </Routes>
